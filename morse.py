0717f329f9002e5112d831d020a052ba2fc6fe8d
satssehgal <satssehgal@gmail.com>
2019-08-14 21:05:33 -0400

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,18 +1,42 @@
+'''
+Created By: Sats Sehgal
+If you are new to python this is a great project for beginners. If you would like to see a walkthrough of the code, 
+please visit the following link:
+
+'''
+
+from playsound import playsound
+import time
+import pyttsx3 as pyttsx
+
 MORSE_CODE_DICT = { ' ':'/', 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....',
                     'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.','O':'---', 'P':'.--.', 'Q':'--.-',
                     'R':'.-.', 'S':'...', 'T':'-','U':'..-', 'V':'...-', 'W':'.--','X':'-..-', 'Y':'-.--', 'Z':'--..',
                     '1':'.----', '2':'..---', '3':'...--','4':'....-', '5':'.....', '6':'-....','7':'--...', '8':'---..', '9':'----.',
                     '0':'-----', ', ':'--..--', '.':'.-.-.-','?':'..--..', '/':'-..-.', '-':'-....-','(':'-.--.', ')':'-.--.-'}
 
+
 def Txt_to_Morse():
     txt = input('Enter Text to Convert to Morse: ')
     code = [MORSE_CODE_DICT[i.upper()] + ' ' for i in txt if i.upper() in MORSE_CODE_DICT.keys()]
-    return(''.join(code))
+    morse=''.join(code)
+    print(morse)
+    for m in morse:
+        if m=='.':
+            playsound('dit.wav')
+        elif m=='-':
+            playsound('dah.wav')
+        else:
+            time.sleep(0.5)
 
 def Morse_to_Txt():
     txt = input('Enter Morse to Convert to Text: ')
     code = [k for i in txt.split() for k,v in MORSE_CODE_DICT.items() if i==v]
-    return(''.join(code))
+    newtxt = ''.join(code)
+    print(newtxt)
+    engine = pyttsx.init()
+    engine.say(newtxt)
+    engine.runAndWait()
 
 print('''\n1 - Convert Text to Morse \n2 - Convert Morse to Text\n3 - Quit\n ''')
 
